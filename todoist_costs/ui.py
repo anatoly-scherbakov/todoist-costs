63108e789cc352760798c703841f7a7f24bde528
Anatoly Scherbakov <altaisoft@gmail.com>
2022-08-06 23:59:12 +0400

    Colorize balances

--- todoist_costs/ui.py
+++ todoist_costs/ui.py
@@ -5,6 +5,20 @@ from rich.table import Table
 
 from todoist_costs.models import FinancialTask
 
+MINIMUM_SAFE_BALANCE = 500
+
+
+def format_balance(balance: float) -> str:
+    formatted_balance = str(round(balance, 2))
+
+    if balance < 0:
+        formatted_balance = f'[red]{formatted_balance}[/red]'
+
+    elif balance < MINIMUM_SAFE_BALANCE:
+        formatted_balance = f'[orange1]{formatted_balance}[/orange1]'
+
+    return formatted_balance
+
 
 def print_report(tasks: Iterable[FinancialTask]):
     table = Table(
@@ -21,7 +35,7 @@ def print_report(tasks: Iterable[FinancialTask]):
             task.name,
             task.description,
             str(round(task.standardized_amount, 2)),
-            str(round(task.balance, 2)),
+            format_balance(task.balance),
         )
 
     Console().print(table)
